ceeaa5cddc2cc5a65bf2ae8f4226cee570ef10d6
Sergei Smirnov <sasmirnov87@gmail.com>
2023-11-19 09:27:58 +0300

    receiving a complete data

--- src/redux/slices/tickets-slice.js
+++ src/redux/slices/tickets-slice.js
@@ -6,10 +6,29 @@ export const fetchSearchId = createAsyncThunk('tickets/fetchSearchId', async () 
   return data.searchId
 })
 
-export const fetchTickets = createAsyncThunk('tickets/fetchTickets', async (searchId) => {
-  const response = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${searchId}`)
-  const data = await response.json()
-  return data
+export const fetchTickets = createAsyncThunk('tickets/fetchTickets', async (searchId, { getState, dispatch }) => {
+  try {
+    const fetchTicketsRecursive = async (receivedTickets = []) => {
+      const response = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${searchId}`)
+      const data = await response.json()
+
+      if (data.tickets.length > 0) {
+        const updatedTickets = [...receivedTickets, ...data.tickets]
+        dispatch(ticketsSlice.actions.updateTickets(updatedTickets))
+        if (!data.stop) {
+          return fetchTicketsRecursive(updatedTickets)
+        }
+      }
+
+      return receivedTickets
+    }
+
+    const updatedTickets = await fetchTicketsRecursive()
+    return updatedTickets
+  } catch (error) {
+    console.error('Error fetching tickets:', error)
+    throw error
+  }
 })
 
 const ticketsSlice = createSlice({
@@ -20,7 +39,9 @@ const ticketsSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  reducers: {},
+  reducers: {
+    updateTickets: (state, action) => ({ ...state, tickets: action.payload }),
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchSearchId.fulfilled, (state, action) => ({ ...state, searchId: action.payload }))
@@ -32,6 +53,15 @@ const ticketsSlice = createSlice({
       }))
       .addCase(fetchTickets.rejected, (state, action) => ({ ...state, error: action.error.message }))
   },
+  /* extraReducers: (builder) => {
+    builder
+      .addCase(fetchSearchId.fulfilled, (state, action) => ({ ...state, searchId: action.payload }))
+      .addCase(fetchTickets.pending, (state) => ({ ...state, isLoading: true }))
+      .addCase(fetchTickets.fulfilled, (state) => ({ ...state, isLoading: false }))
+      .addCase(fetchTickets.rejected, (state, action) => ({ ...state, error: action.error.message }))
+  }, */
 })
+
+export const { updateTickets } = ticketsSlice.actions
 
 export default ticketsSlice.reducer
