e879c857194e12f5f8479be291935a9f9717d539
Sergei Smirnov <sasmirnov87@gmail.com>
2023-11-19 21:51:39 +0300

    sort completed

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -11,6 +11,7 @@ function List() {
   const dispatch = useDispatch()
   const tickets = useSelector((state) => state.tickets.tickets)
   const filters = useSelector((state) => state.filters.filters)
+  const sortBy = useSelector((state) => state.sorting.sortBy)
   const [visibleTickets, setVisibleTickets] = useState(5)
 
   useEffect(() => {
@@ -18,7 +19,24 @@ function List() {
   }, [dispatch, tickets, filters, visibleTickets])
 
   const filteredTickets = useSelector((state) => state.filters.filteredTickets)
-  const displayedTickets = filteredTickets.slice(0, visibleTickets)
+
+  const getTotalDuration = useMemo(
+    () => (ticket) => ticket.segments.reduce((total, segment) => total + segment.duration, 0),
+    []
+  )
+  const calculateOptimalValue = useMemo(
+    () => (ticket) => ticket.price * (1 + ticket.segments[0].stops.length + ticket.segments[1].stops.length),
+    []
+  )
+
+  const sortFunctions = {
+    cheap: (a, b) => a.price - b.price,
+    fast: (a, b) => getTotalDuration(a) - getTotalDuration(b),
+    optimal: (a, b) => calculateOptimalValue(a) - calculateOptimalValue(b),
+  }
+
+  const sortedTickets = [...filteredTickets].sort(sortFunctions[sortBy] || (() => 0))
+  const displayedTickets = sortedTickets.slice(0, visibleTickets)
 
   const handleShowMore = () => {
     setVisibleTickets((prevVisibleTickets) => prevVisibleTickets + 5)
